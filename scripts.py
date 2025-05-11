3d28d7509a8180b8ab5c3e078b14ce3db573069e
Paul <hidden80@mail.ru>
2025-05-12 01:39:14 +0500

    Update correcting scripts.py

--- scripts.py
+++ scripts.py
@@ -2,11 +2,43 @@ import random
 from datacenter.models import Schoolkid, Mark, Chastisement, Commendation, Lesson
 from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 
+commendations = [
+    'Молодец!',
+    'Отлично!',
+    'Хорошо!',
+    'Гораздо лучше, чем я ожидал!',
+    'Ты меня приятно удивил!',
+    'Великолепно!',
+    'Прекрасно!',
+    'Ты меня очень обрадовал!',
+    'Именно этого я давно ждал от тебя!',
+    'Сказано здорово – просто и ясно!',
+    'Ты, как всегда, точен!',
+    'Очень хороший ответ!',
+    'Талантливо!',
+    'Ты сегодня прыгнул выше головы!',
+    'Я поражен!',
+    'Уже существенно лучше!',
+    'Потрясающе!',
+    'Замечательно!',
+    'Прекрасное начало!',
+    'Так держать!',
+    'Ты на верном пути!',
+    'Здорово!',
+    'Это как раз то, что нужно!',
+    'Я тобой горжусь!',
+    'С каждым разом у тебя получается всё лучше!',
+    'Мы с тобой не зря поработали!',
+    'Я вижу, как ты стараешься!',
+    'Ты растешь над собой!',
+    'Ты многое сделал, я это вижу!',
+    'Теперь у тебя точно все получится!',
+]
+
 
 def get_schoolkid_by_name(full_name):
     try:
-        schoolkid = Schoolkid.objects.get(full_name__icontains=full_name)
-        return schoolkid
+        return Schoolkid.objects.get(full_name__icontains=full_name)
     except Schoolkid.DoesNotExist:
         raise ValueError("Ученик с таким именем не найден!")
     except Schoolkid.MultipleObjectsReturned:
@@ -14,51 +46,14 @@ def get_schoolkid_by_name(full_name):
 
 
 def fix_marks(schoolkid):
-    bad_points = Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3])
-    for point in bad_points:
-        point.points = random.choice([4, 5])
-        point.save()
+    Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3]).update(points=5)
 
 
 def remove_chastisements(schoolkid):
-    chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
-    chastisements.delete()
+    Chastisement.objects.filter(schoolkid=schoolkid).delete()
 
 
 def create_random_commendation(schoolkid, subject_title):
-    commendations = [
-        'Молодец!',
-        'Отлично!',
-        'Хорошо!',
-        'Гораздо лучше, чем я ожидал!',
-        'Ты меня приятно удивил!',
-        'Великолепно!',
-        'Прекрасно!',
-        'Ты меня очень обрадовал!',
-        'Именно этого я давно ждал от тебя!',
-        'Сказано здорово – просто и ясно!',
-        'Ты, как всегда, точен!',
-        'Очень хороший ответ!',
-        'Талантливо!',
-        'Ты сегодня прыгнул выше головы!',
-        'Я поражен!',
-        'Уже существенно лучше!',
-        'Потрясающе!',
-        'Замечательно!',
-        'Прекрасное начало!',
-        'Так держать!',
-        'Ты на верном пути!',
-        'Здорово!',
-        'Это как раз то, что нужно!',
-        'Я тобой горжусь!',
-        'С каждым разом у тебя получается всё лучше!',
-        'Мы с тобой не зря поработали!',
-        'Я вижу, как ты стараешься!',
-        'Ты растешь над собой!',
-        'Ты многое сделал, я это вижу!',
-        'Теперь у тебя точно все получится!',
-    ]
-
     lesson = Lesson.objects.filter(
         year_of_study=schoolkid.year_of_study,
         group_letter=schoolkid.group_letter,
@@ -90,8 +85,8 @@ def main():
         fix_marks(schoolkid)
         remove_chastisements(schoolkid)
         create_random_commendation(schoolkid, subject_title)
-    except ValueError as e:
-        print(f"Ошибка: {e}")
+    except ValueError as error:
+        print(f"Ошибка: {error}")
 
 
 main()
